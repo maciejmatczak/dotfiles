f21952fd627e6ec8dfcda7930191ef48442bf1eb
Maciej Matczak <maciek.matczak@gmail.com>
2021-02-13 14:27:05 +0100

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,1 @@
-function wal2
-	wal -n $argv
-	feh --bg-fill --no-xinerama (cat $HOME/.cache/wal/wal)
-	# nitrogen --set-zoom $HOME/.cache/wal/wal
-end
-
-alias dot='/usr/bin/git --git-dir=$HOME/.dot-git/ --work-tree=$HOME'
-
 cat ~/.cache/wal/sequences
